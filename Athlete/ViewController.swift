055e3f697cfcf555855dcdd4c34b71bde0c6086d
Asad <Asadullah9555@gmail.com>
2024-10-30 13:22:08 +0500

    Initial Commit

---
+++ Athlete/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Athlete
+//
+//  Created by Mac on 30/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AthleteTests/AthleteTests.swift
@@ -0,0 +1,17 @@
+//
+//  AthleteTests.swift
+//  AthleteTests
+//
+//  Created by Mac on 30/10/2024.
+//
+
+import Testing
+@testable import Athlete
+
+struct AthleteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
